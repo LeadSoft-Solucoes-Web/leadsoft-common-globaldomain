866fcc3e496ff29b4a7e9ec0ba00e2daa01a2d2a
Tavares, Lucas (CONTRACTOR) <lucas.tavares@bp.com>
2025-10-06 18:04:22 -0300

    Atualizando nugets e incluindo novo construtor no DTO Paged List Response.

--- LeadSoft.Common.GlobalDomain/DTOs/DTOPagedListResponse.Methods.cs
+++ LeadSoft.Common.GlobalDomain/DTOs/DTOPagedListResponse.Methods.cs
@@ -22,6 +22,17 @@ namespace LeadSoft.Common.GlobalDomain.DTOs
         }
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="DTOPagedListResponse{T}"/> class with the specified list, paged
+        /// request, and total results count.
+        /// </summary>
+        /// <param name="aList">The list of items to include in the paged response.</param>
+        /// <param name="pagedRequest">The paged request details, including page number and size.</param>
+        /// <param name="aTotalResults">The total number of results available across all pages.</param>
+        public DTOPagedListResponse(IEnumerable<T> aList, PagedRequest pagedRequest, long aTotalResults) : base(aList, pagedRequest, aTotalResults)
+        {
+        }
+
+        /// <summary>
         /// Constructor based on an existing list, that sets inner base constructor with informed data.
         /// </summary>
         /// <param name="aList">List of objects</param>
@@ -33,39 +44,18 @@ namespace LeadSoft.Common.GlobalDomain.DTOs
         }
 
         /// <summary>
-        /// Gets header paging intomation serialized object in Json string
+        /// Generates a sanitized paging header as a JSON string for the current page context.
         /// </summary>
-        /// <typeparam name="TRequest">Request type</typeparam>
-        /// <param name="aUrl">Method Url</param>
-        /// <param name="aRouteName">Name of current route</param>
-        /// <param name="aDtoPagedRequestInheritance">DTO paged request inheritance to keep original params on query</param>
-        /// <returns></returns>
-        [Obsolete("This method is obsolete and insecure. Use GetSanitizedPagingHeader instead.")]
-        public string GetPagingHeader<TRequest>(IUrlHelper aUrl, string aRouteName, TRequest aDtoPagedRequestInheritance) where TRequest : PagedRequest
-        {
-            if (!typeof(TRequest).IsSubclassOf(typeof(PagedRequest)))
-                throw new ArgumentException(string.Format(ApplicationStatusMessage.ArgumentMustInheritFromDTOPagedRequest, nameof(aDtoPagedRequestInheritance)));
-
-            PagingMetadata metadata = new(CurrentPage, PageSize, TotalResults, TotalPages);
-
-            if (HasPreviousPage)
-                metadata.SetPreviousPageLink(aUrl.Link(aRouteName, aDtoPagedRequestInheritance.GetPrevious()));
-
-            if (HasNextPage)
-                metadata.SetNextPageLink(aUrl.Link(aRouteName, aDtoPagedRequestInheritance.GetNext()));
-
-            return metadata.SetCurrentPageLink(aUrl.Link(aRouteName, aDtoPagedRequestInheritance))
-                           .ToJson();
-        }
-
-        /// <summary>
-        /// Gets sanitized header paging intomation serialized object in Json string
-        /// </summary>
-        /// <typeparam name="TRequest">Request type</typeparam>
-        /// <param name="aUrl">Method Url</param>
-        /// <param name="aRouteName">Name of current route</param>
-        /// <param name="aDtoPagedRequestInheritance">DTO paged request inheritance to keep original params on query</param>
-        /// <returns></returns>
+        /// <remarks>The method constructs a <see cref="PagingMetadata"/> object using the current page
+        /// context and generates links for the previous and next pages if they exist. The resulting metadata is
+        /// serialized to a JSON string.</remarks>
+        /// <typeparam name="TRequest">The type of the paged request, which must inherit from <see cref="PagedRequest"/>.</typeparam>
+        /// <param name="aUrl">The URL helper used to generate links for pagination.</param>
+        /// <param name="aRouteName">The name of the route to use for generating pagination links.</param>
+        /// <param name="aDtoPagedRequestInheritance">The paged request object containing pagination parameters.</param>
+        /// <returns>A JSON string representing the paging metadata, including links to the current, previous, and next pages if
+        /// applicable.</returns>
+        /// <exception cref="ArgumentException">Thrown if <typeparamref name="TRequest"/> does not inherit from <see cref="PagedRequest"/>.</exception>
         public string GetSanitizedPagingHeader<TRequest>(IUrlHelper aUrl, string aRouteName, TRequest aDtoPagedRequestInheritance) where TRequest : PagedRequest
         {
             if (!typeof(TRequest).IsSubclassOf(typeof(PagedRequest)))
