a3bb959f8a732e468a5d52d8d46018774e56536c
Tavares, Lucas (CONTRACTOR) <lucas.tavares@bp.com>
2025-10-07 09:26:00 -0300

    CNPJ e CPF como DataAttributes

---
+++ LeadSoft.Common.GlobalDomain/Entities/Infos/Documents/CNPJAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeadSoft.Common.GlobalDomain.Entities.Infos.Documents
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class CNPJAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is null)
+                return true;
+
+            string str = value.ToString()!;
+
+            if (str.IsCpf())
+                return true;
+
+            ErrorMessage ??= "CPF inválido.";
+            return false;
+        }
+    }
+}
---
+++ LeadSoft.Common.GlobalDomain/Entities/Infos/Documents/CPFAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeadSoft.Common.GlobalDomain.Entities.Infos.Documents
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class CPFAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is null)
+                return true;
+
+            string str = value.ToString()!;
+
+            if (str.IsCpf())
+                return true;
+
+            ErrorMessage ??= "CPF inválido.";
+            return false;
+        }
+    }
+}
